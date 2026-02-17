b7514e95f6a46ba81535328a62c7ebd78b0671b7
drfoxg <drfoxg@gmail.com>
2026-02-17 11:42:13 +0300

    Fix:  убрал return для brain-games, т.к. не используется

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,9 @@ namespace App\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome(): string
+function welcome(): void
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-
-    return $name;
 }
